9e0c4bde2e3d508b97dbb71d2550e08ef8869384
Jyun-De <e1044421301@gm.lhu.edu.tw>
2017-11-24 12:57:04 +0800

    unity_cs_00

---
+++ unity_cs_00/my_script/hero.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hero : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//修改gameobject的座標
+		//不斷的x加上0.01
+		gameObject.transform.Translate(0.01f, 0, 0);
+	}
+}
